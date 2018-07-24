5f09b5be3fe4e9db79d25a35eb67fd3e56aeb97e
fe0b6 <spirit-dnk@yandex.ru>
2018-07-24 08:45:05 +0300

    first commit

---
+++ ampliferapi.go
@@ -0,0 +1,90 @@
+package ampliferapi
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+// GetProjects - Получаем список проектов
+func (api *API) GetProjects() (ans GetProjectsAns, err error) {
+
+	res, err := api.rq("/api/v1/projects", map[string]string{})
+	if err != nil {
+		log.Println("[error]", err)
+		return
+	}
+
+	err = json.Unmarshal(res.Result, &ans)
+	if err != nil {
+		log.Println("[error]", err)
+		return
+	}
+
+	return
+}
+
+// GetProjectPosts - Получаем список постов проекта
+func (api *API) GetProjectPosts(projectID int64, params map[string]string) (ans GetProjectPostsAns, err error) {
+
+	res, err := api.rq(fmt.Sprintf("/api/v1/projects/%d/posts", projectID), params)
+	if err != nil {
+		log.Println("[error]", err)
+		return
+	}
+
+	err = json.Unmarshal(res.Result, &ans)
+	if err != nil {
+		log.Println("[error]", err)
+		return
+	}
+
+	return
+}
+
+func (api *API) rq(link string, params map[string]string) (res rqAns, err error) {
+	q := url.Values{}
+	q.Add("access_token", api.AccessToken)
+	for k, v := range params {
+		q.Add(k, v)
+	}
+
+	resp, err := http.Get(link + "?" + q.Encode())
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+	if err != nil {
+		log.Println("[error]", err)
+		return
+	}
+
+	if resp.StatusCode != 200 {
+		err = fmt.Errorf("Status: %s, Code: %d", resp.Status, resp.StatusCode)
+		log.Println("[error]", err)
+		return
+	}
+
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("[error]", err)
+		return
+	}
+
+	err = json.Unmarshal(content, &res)
+	if err != nil {
+		log.Println("[error]", err)
+		return
+	}
+
+	if !res.OK {
+		err = errors.New("bad ans")
+		log.Println("[error]", string(content))
+		return
+	}
+
+	return
+}
---
+++ type.go
@@ -0,0 +1,51 @@
+package ampliferapi
+
+import "encoding/json"
+
+// API - объкт api
+type API struct {
+	AccessToken string
+}
+
+type rqAns struct {
+	OK     bool            `json:"ok"`
+	Result json.RawMessage `json:"result"`
+}
+
+// GetProjectsAns - объект проектов
+type GetProjectsAns struct {
+	Accounts []Account `json:"accounts"`
+}
+
+// GetProjectPostsAns - Объект постов
+type GetProjectPostsAns struct {
+	Posts      []Post         `json:"posts"`
+	Pagination map[string]int `json:"pagination"`
+}
+
+// Account - Объект аккаунта
+type Account struct {
+	ID          int64  `json:"id"`
+	Name        string `json:"name"`
+	URL         string `json:"url"`
+	Avatar      string `json:"avatar"`
+	Network     string `json:"network"`
+	NetworkAbbr string `json:"networkAbbr"`
+	Active      bool   `json:"active"`
+	Publishable bool   `json:"publishable"`
+}
+
+// Post - объект поста
+type Post struct {
+	ID             int64             `json:"id"`
+	Clicks         int               `json:"clicks"`
+	Likes          int               `json:"likes"`
+	Shares         int               `json:"shares"`
+	Comments       int               `json:"comments"`
+	UniqueViews    int               `json:"uniqueViews"`
+	FanUniqueViews int               `json:"fanUniqueViews"`
+	TotalViews     int               `json:"totalViews"`
+	VideoPlays     int               `json:"videoPlays"`
+	States         map[string]string `json:"states"`
+	Publications   map[string]string `json:"publications"`
+}
